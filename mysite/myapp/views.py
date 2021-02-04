73f421775ed100f3567bd6b4df884b1c589f53bc
Bryan Dixon <javawolfpack@gmail.com>
2021-02-04 15:18:42 -0800

    Django Basics

---
+++ mysite/myapp/views.py
@@ -0,0 +1,15 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+# Create your views here.
+def index(request, page=0):
+    content = list(range(page*10,page*10+10,1))
+    context = {
+        "title":"CINS 465 Title",
+        "variable": "Awesome Template",
+        "items":content,
+        "next":page+1,
+        "prev":page-1
+    }
+    return render(request, "index.html", context=context)
+    # return HttpResponse("Hello World")
