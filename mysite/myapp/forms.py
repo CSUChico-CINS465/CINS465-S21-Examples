29ce1ae62a4ac26823ba0e7071a443ada7c234bb
Bryan Dixon <javawolfpack@gmail.com>
2021-02-11 15:02:29 -0800

    Forms

---
+++ mysite/myapp/forms.py
@@ -0,0 +1,31 @@
+from django import forms
+from django.core.validators import validate_slug
+
+from . import models
+
+def must_be_caps(value):
+    if not value.isupper():
+        raise forms.ValidationError("Not all upper case")
+    # Always return the cleaned data
+    return value
+
+def must_be_bob(value):
+    if not value.endswith("BOB"):
+        raise forms.ValidationError("Must end with BOB")
+    return value 
+
+
+
+class SuggestionForm(forms.Form):
+    suggestion = forms.CharField(
+        label="Suggestion",
+        required=True,
+        max_length=240,
+        validators=[validate_slug,must_be_caps, must_be_bob ]
+    )
+
+    def save(self):
+        suggestion_instance = models.SuggestionModel()
+        suggestion_instance.suggestion = self.cleaned_data["suggestion"]
+        suggestion_instance.save()
+        return suggestion_instance
--- mysite/myapp/views.py
+++ mysite/myapp/views.py
@@ -2,14 +2,23 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 from . import models
+from . import forms
 
 # Create your views here.
 def index(request):
+    if request.method == "POST":
+        suggestion_form = forms.SuggestionForm(request.POST)
+        if suggestion_form.is_valid():
+            suggestion_form.save()
+            suggestion_form = forms.SuggestionForm()
+    else:
+        suggestion_form = forms.SuggestionForm()
     suggestion_objects = models.SuggestionModel.objects.all()
     context = {
         "title":"CINS 465 Title",
         "variable": "Awesome Template",
         "suggestions":suggestion_objects,
+        "suggestion_form":suggestion_form,
     }
     return render(request, "index.html", context=context)
     # return HttpResponse("Hello World")
