b91c6bbddaf6fd92ee4c10ed244e0d7fbab01cfd
Bryan Dixon <javawolfpack@gmail.com>
2021-02-16 15:05:29 -0800

    Login & Top Bar

--- mysite/myapp/urls.py
+++ mysite/myapp/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
+from django.contrib.auth import views as auth_views
 
 from . import views #convention
 
 
 urlpatterns = [
     path('', views.index),
+    path('login/', auth_views.LoginView.as_view()),
 ]
