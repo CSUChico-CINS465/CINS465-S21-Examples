3a78b5a94b63fa81953a6c648d2bb61cad906066
Bryan Dixon <javawolfpack@gmail.com>
2021-03-04 15:02:19 -0800

    Test Comment and move coverage

--- mysite/myapp/tests.py
+++ mysite/myapp/tests.py
@@ -16,3 +16,17 @@ class SuggestionTestCase(TestCase):
         cat = models.SuggestionModel.objects.get(suggestion="cat")
         self.assertEqual(str(lion), "john lion")
         self.assertEqual(str(cat), "john cat")
+
+class CommentTestCase(TestCase):
+    def setUp(self):
+        user = User.objects.create_user('john', 'lennon@thebeatles.com', 'johnpassword')
+        user.save()
+        user_instance = User.objects.get(id=1)
+        sugg = models.SuggestionModel.objects.create(suggestion="lion", author=user_instance)
+        models.CommentModel.objects.create(comment="cat", suggestion=sugg, author=user_instance)
+
+    def test_comment_to_string(self):
+        cat = models.CommentModel.objects.get(comment="cat")
+        # cat = models.SuggestionModel.objects.get(suggestion="cat")
+        # self.assertEqual(str(lion), "john lion")
+        self.assertEqual(str(cat), "john cat")
