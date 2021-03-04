e3ae88aac87d918acefa0f9dfaec1c8a78bc94f0
Bryan Dixon <javawolfpack@gmail.com>
2021-03-04 14:37:34 -0800

    Simple Tests

--- mysite/myapp/tests.py
+++ mysite/myapp/tests.py
@@ -1,3 +1,18 @@
-# from django.test import TestCase
+from django.test import TestCase
+from django.contrib.auth.models import User
+from . import models
 
 # Create your tests here.
+class SuggestionTestCase(TestCase):
+    def setUp(self):
+        user = User.objects.create_user('john', 'lennon@thebeatles.com', 'johnpassword')
+        user.save()
+        user_instance = User.objects.get(id=1)
+        models.SuggestionModel.objects.create(suggestion="lion", author=user_instance)
+        models.SuggestionModel.objects.create(suggestion="cat", author=user_instance)
+
+    def test_suggestion_to_string(self):
+        lion = models.SuggestionModel.objects.get(suggestion="lion")
+        cat = models.SuggestionModel.objects.get(suggestion="cat")
+        self.assertEqual(str(lion), "john lion")
+        self.assertEqual(str(cat), "john cat")
