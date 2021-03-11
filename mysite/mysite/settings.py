a298bbe84caad72990d6e1dc710291384f4a7e39
Bryan Dixon <javawolfpack@gmail.com>
2021-03-11 23:16:33 +0000

    Cloud SQL, Daphne, Static Files via Caddy

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -78,10 +78,18 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
 DATABASES = {
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.sqlite3',
+    #     'NAME': BASE_DIR / 'db.sqlite3',
+    # }
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'django',
+        'USER': 'django',
+        'PASSWORD': 'django',
+        'HOST': '34.68.54.48',
+        'PORT': '5432'
+    },
 }
 
 
